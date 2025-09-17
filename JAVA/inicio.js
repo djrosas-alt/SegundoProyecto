93020cbd1524d1ee3bcdc84b99a6e88d1bdfb090
djrosas-alt <djrosas@uniboyaca.edu.co>
2025-09-17 16:36:22 -0500

    inicio con js

---
+++ JAVA/inicio.js
@@ -0,0 +1,69 @@
+function iniciar(event){
+    imagen.src = 'IMAGENES/principal_0.png'
+    btn.disabled = true;
+    cont_errores = 0;
+    cont_aciertos = 0; 
+
+    const parrafo = id('palabra');
+    parrafo.innerHTML = ''; 
+
+    const cant_palabras = palabra_adivinar.length;
+    const valorRandom = obtener_valor( 0, cant_palabras );
+
+    palabraFinal = palabra_adivinar[ valorRandom  ];
+    console.log( palabraFinal );
+    const cant_letras = palabraFinal.length;
+
+    for( let i = 0; i < btn_abecedario.length ; i++ ){
+        btn_abecedario[ i ].disabled = false;
+    }
+
+    for( let i = 0; i < cant_letras; i++ ){
+        const span = document.createElement( 'span' );
+        parrafo.appendChild( span );
+    }
+}
+for( let i = 0; i < btn_abecedario.length ; i++ ){
+    btn_abecedario[ i ].addEventListener( 'click', letras);
+}
+
+function letras(event){
+    const span = document.querySelectorAll( '#palabra span' );
+    const button = event.target; 
+    button.disabled = true;
+
+    const letra = button.innerHTML.toLowerCase( );
+    const palabra = palabraFinal.toLowerCase( );
+    let acerto = false;
+    for( let i = 0; i < palabra.length;  i++ ){
+        if( letra == palabra[i] ){
+            span[i].innerHTML = letra;
+            cont_aciertos++;
+            acerto = true;
+        }
+    }
+
+    if( acerto == false ){
+        cont_errores++;
+        const source = `imagenes/principal_${cont_errores}.jpg` ;
+        imagen.src = source;
+    }
+
+ if( cont_errores == 8 ){
+        id('result').innerHTML ="Perdiste, La palabra era " + palabraFinal;
+        game_over( );
+    }else if( cont_aciertos == palabraFinal.length ){
+        id('result').innerHTML = "Super bien!! ganaste!!";
+        game_over( );
+    }
+    console.log( "¿La letra " + letra + " existe en " + palabra + "?: " + acerto );
+}
+
+function game_over( ){
+    for( let i = 0; i < btn_abecedario.length ; i++ ){
+        btn_abecedario[ i ].disabled = true;
+    }
+    btn.disabled = false;
+}
+
+game_over( );
